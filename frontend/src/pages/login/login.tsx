d086ea074c69b2232e3d54a83cda7d4dfbb2644b
Ana Paula Ferreira <bielgabrielfs61@gmail.com>
2024-03-24 14:47:30 -0300

    feat: start styling

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -3,13 +3,12 @@ import './login.css'
 const Login = () => {
     return (
         <div className="container">
-            <div className="box-login">
-                <form action="" method="post">
-                    <input type="text" name="nickname" id="nickname" />
-                    <input type="color" name="input-color" id="input-color" />
-                    <button type="submit"></button>
-                </form>
-            </div>
+            <form action="" method="post">
+                <button>Upload a file</button>
+                <input type="text" name="nickname" id="nickname" />
+                <input type="color" name="input-color" id="input-color" />
+                <button type="submit">Join Chat</button>
+            </form>
         </div>
     );
 }

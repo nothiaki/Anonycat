da5572d8efbe9a197e18cf9f0b27a6e5e80902fd
nothiaki <celsohenrique367@gmail.com>
2024-05-01 14:57:22 -0300

    refactor: remove profile picture for now

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -10,7 +10,7 @@ const inputSchema = z.object({
   name: z.string()
 });
 
-type FormData = z.infer<typeof inputSchema>;
+type User = z.infer<typeof inputSchema>;
 
 export const Login = () => {
   const navigate = useNavigate();
@@ -18,7 +18,7 @@ export const Login = () => {
     resolver: zodResolver(inputSchema)
   });
 
-  const joinChat = async (data: FormData) => {
+  const joinChat = async (data: User) => {
     const res = await fetch('http://localhost:3000/user', {
       method: 'POST',
       headers: {
@@ -40,37 +40,26 @@ export const Login = () => {
     navigate('/chat');
   }, [navigate]);
 
-  const uploadPicture = (evt: any) => {
-    evt.preventDefault();
-    alert('This feature will coming soon!');
-  };
-
   return (
     <div
-      className='w-screen min-h-screen p-8 flex flex-col items-center justify-center gap-8 bg-background text-text md:flex-row md:p-32'>
+      className='w-screen min-h-screen p-12 flex flex-col items-center justify-center gap-8 bg-background text-text md:flex-row md:p-36'>
       <div className='flex flex-col gap-10 md:w-1/2'>
         <h1 className='text-4xl font-medium'>Join now on <br />Anonycat! <br />Shh.</h1>
         <p>Welcome to our website where you can create a temporary account and send anonymous messages, which are deleted every 40 minutes.</p>
         <p>Remember to read our <a className='text-link' href='#'>privacy policy terms</a>.</p>
       </div>
 
-      <form className='w-full flex flex-col gap-10 md:w-1/2' onSubmit={handleSubmit(joinChat)}>
-        <div className='flex items-end gap-4'>
-          <img className='w-28 h-28 rounded' src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' />
-          <div className='flex flex-col justify-around'>
-            <label>
-              <h3>Color:</h3>
-              <input className='w-full border-none bg-background hover:cursor-pointer'
-                type="color" {...register('color')}
-              />
-            </label>
-            <Button func={uploadPicture} content='Picture' />
-          </div>
-        </div>
+      <form className='w-full flex flex-col gap-12 md:w-1/2' onSubmit={handleSubmit(joinChat)}>
         <label>
           <h3>Username:</h3>
           <input className='w-full p-1 mt-0.5 bg-[#ffffff00] border-b-2 border-primary focus:outline-none'
             type='text' {...register('name')}
+          />
+        </label>
+        <label>
+          <h3>Color:</h3>
+          <input className='w-full mt-2 border-none bg-background hover:cursor-pointer'
+            type="color" {...register('color')}
           />
         </label>
         <Button content='Join Chat' />

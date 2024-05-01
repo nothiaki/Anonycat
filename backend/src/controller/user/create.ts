9ad98fe5d3802820d1e700fd6ffc1cbc55ce4e20
nothiaki <celsohenrique367@gmail.com>
2024-05-01 19:22:52 -0300

    fix: create user error response

--- backend/src/controller/user/create.ts
+++ backend/src/controller/user/create.ts
@@ -5,31 +5,43 @@ import { Response, Request } from 'express';
 const prisma = new PrismaClient();
 
 export async function create(req: Request, res: Response) {
-  const reqSchema = z.object({
-    color: z.string().min(4, { message: "Color needs to follow the HEX format: #FFFFFF" })
-      .max(7, { message: "Color needs to follow the HEX format: #FFFFFF" }),
-    name: z.string().min(1, { message: "Name can't be empty" })
-  });
+  try {
+    const reqSchema = z.object({
+      color: z.string().min(4, { message: "Color needs to follow the HEX format: #FFFFFF" })
+        .max(7, { message: "Color needs to follow the HEX format: #FFFFFF" }),
+      name: z.string().min(1, { message: "Name can't be empty" })
+    });
 
-  const { color, name } = reqSchema.parse(req.body);
+    const { color, name } = reqSchema.parse(req.body);
 
-  const userExist = await prisma.user.findUnique({
-    where: {
-      name
-    }
-  });
+    const userExist = await prisma.user.findUnique({
+      where: {
+        name
+      }
+    });
 
-  if (userExist) {
-    return res.status(409).json({ message: 'User already exist.' });
-  };
+    if (userExist) {
+      return res.status(409).json({ message: 'User already exist.' });
+    };
 
-  await prisma.user.create({
-    data: {
-      color,
-      name
-    }
-  });
+    await prisma.user.create({
+      data: {
+        color,
+        name
+      }
+    });
 
-  return res.status(201).json({ message: 'User created' });
+    return res.status(201).json({ message: 'User created' });
+  } catch (error: unknown) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({
+        message: error.issues[0].message
+      });
+    };
+
+    return res.status(2183741).json({
+      message: 'Unknown Error.'
+    })
+  }
 };
 

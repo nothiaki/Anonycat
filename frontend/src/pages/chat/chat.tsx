104fa5d45499ce28221402b4bfc8c7e9ead27a54
nothiaki <celsohenrique367@gmail.com>
2024-05-02 11:14:00 -0300

    feat: logout button

--- frontend/src/pages/chat/chat.tsx
+++ frontend/src/pages/chat/chat.tsx
@@ -5,6 +5,7 @@ import { Message } from '../../types/Message';
 import { isAuth } from '../../services/auth';
 import { NotFound } from '../notFound/notFound';
 import { useNavigate } from 'react-router-dom';
+import { ArrowBigLeft, ArrowBigUp } from 'lucide-react';
 
 const socket = io('http://localhost:3000');
 
@@ -36,16 +37,24 @@ export const Chat = () => {
     return (
       <div
         className='w-screen min-h-screen p-8 flex flex-col items-center justify-center gap-8 bg-background text-text md:p-32'>
-        <header className='bg-link'>
-          <h2 onClick={signout}>back</h2>
+        <header className='w-full p-4 absolute top-0'>
+          <div className='w-fit p-1 bg-primary rounded hover:cursor-pointer' onClick={signout}>
+            <ArrowBigLeft />
+          </div>
         </header>
-        <input className='w-full p-1 mt-0.5 bg-[#ffffff00] border-b-2 border-primary focus:outline-none'
-          type='text' placeholder='Send a message...' onChange={(evt) => { setInput(evt.target.value) }}
-        />
-        <Button func={sendMessage} content='Send' />
-        <p>messages...</p>
-        <p>messages...</p>
-        <p>{getMessage}</p>
+        <main>
+          <p>messages...</p>
+          <p>messages...</p>
+          <p>{getMessage}</p>
+        </main>
+        <footer className='absolute bottom-0 p-4 flex gap-4'>
+          <input className='w-full p-2 bg-primary rounded focus:outline-none'
+            type='text' placeholder='Send a message...' onChange={(evt) => { setInput(evt.target.value) }}
+          />
+          <div className='flex items-center justify-center p-1 bg-primary rounded hover:cursor-pointer' onClick={signout}>
+            <ArrowBigUp />
+          </div>
+        </footer>
       </div>
     );
   } else {
